74a297b8763231d0b3d6a3ba32c4224bc464b9be
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-20 21:17:19 +0530

    saving Ar4

---
+++ Ar4.java
@@ -0,0 +1,15 @@
+class Ar4{
+	public static void main(String [] as){
+		long []dog={2345672,345678545667l,45467574546l,4565747674758l,345698767,-342344534,45465766,9867777563435l,-345464647};
+		System.out.println(dog[3]);
+		System.out.println(dog[8]);
+		System.out.println(dog[5]);
+		System.out.println(dog[1]);
+		System.out.println(dog[6]);
+		System.out.println(dog[2]);
+		System.out.println(dog[4]);
+		System.out.println(dog[7]);
+		
+	}
+
+}
